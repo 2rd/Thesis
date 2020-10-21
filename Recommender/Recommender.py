c680d69be1f6d1e2316c57d61c3d03fec18bc52b
2rd <tordkvifte@outlook.com>
2020-10-21 13:23:43 +0200

    api first commit

--- Recommender/Recommender.py
+++ Recommender/Recommender.py
@@ -3,22 +3,33 @@ from lightfm import LightFM
 
 class Recommender:
 
-    def __init__(self, item_dict, user_dict, item_features, interactions):
+    def __init__(self, item_dict={}, user_dict={}, item_features=[], interactions[], user_id=None):
         self.recommendations = {}
         self.item_dict = item_dict
         self.user_dict = user_dict
+        self.interactions = interactions
+        self.user_id = user_id
+
+    def get_interactions(self):
+
+        return self.interactions
+
+    def get_user_dict(self):
+
+        return self.user_dict
 
     def explain(self, recommendations, interactions, items):
         '''
         Give explanations based on the features of
-        the recommended items, and the user's interactions
+        the recommended items and the user's interactions
         '''
+        known_positives = self.known_positives()
         explanations = {}
 
         return explanations
 
-    def known_positives(self, interactions, items, user_id, user_dict):
-        user_index = user_dict[user_id]
+    def known_positives(self):
+        user_index = self.get_user_dict()[user_id]
         known_positives = items[interactions[user_index].indices]
 
         return known_positives
